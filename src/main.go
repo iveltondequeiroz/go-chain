772b16c0f7caedb1e0c81a629b9326280d8158e1
@ivelton2018 <ivelton2018@gmail.com>
2018-10-01 09:21:20 -0400

    first commit

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main 
+
+import (
+	"fmt"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	fmt.Println("hello")
+}
